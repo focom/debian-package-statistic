856e9be019527811e022e23e8e84763f83f93ad2
Pierre Valentin <pierre.valentin@wrnch.ai>
2020-05-27 20:11:11 -0400

    prospector

--- package_statistic_test.py
+++ package_statistic_test.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import tempfile
-from unittest.mock import patch, call
+from unittest.mock import call, patch
+
 import pytest
-import pytest_mock
+
 import package_statistic
 
 
@@ -23,24 +24,30 @@ def test_process_content_file():
         result = package_statistic.process_content_file(content_file)
         assert expected_result == result
 
-@patch('builtins.print')
+
+@patch("builtins.print")
 def test_std_output(mocked_print):
     with open("content_file_test.txt") as content_file:
-      result = package_statistic.process_content_file(content_file)
+        result = package_statistic.process_content_file(content_file)
     package_statistic.print_top_10_packages(result)
-    assert mocked_print.mock_calls == [call('1. mate-applets-common\n\t2'), call('2. mate-user-guide\n\t1')]
+    assert mocked_print.mock_calls == [
+        call("1. mate-applets-common\n\t2"),
+        call("2. mate-user-guide\n\t1"),
+    ]
+
 
 def test_download_file():
     with tempfile.TemporaryDirectory() as tempdir:
         arch = "arm64"
         try:
             file_path = package_statistic.download_file(tempdir, arch)
-        except:
+        except Exception:
             pytest.fail("Could not download the file")
 
         assert os.path.isfile(file_path)
 
-@patch('builtins.print')
+
+@patch("builtins.print")
 def test_end_to_end(mocked_print):
     sys.argv = ["test", "arm64"]
     with pytest.raises(SystemExit) as pytest_wrapped_e:
